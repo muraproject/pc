f6b92c379711d91efe37d63ee2225ee621365f66
muraproject <rifaiali1210@gmail.com>
2024-10-07 14:49:29 +0700

    a

--- cat/logout.php
+++ cat/logout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
+
+// Unset all session variables
 $_SESSION = array();
+
+// Destroy the session cookie
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -8,7 +12,20 @@ if (ini_get("session.use_cookies")) {
         $params["secure"], $params["httponly"]
     );
 }
+
+// Destroy the session
 session_destroy();
+
+// Remove remember_me cookie if it exists
+if (isset($_COOKIE['remember_me'])) {
+    unset($_COOKIE['remember_me']);
+    setcookie('remember_me', '', time() - 3600, '/'); // empty value and old timestamp
+}
+
+// Clear any output that might have been sent
+ob_clean();
+
+// Redirect to login page
 header("Location: /pc/cat/login.php");
 exit();
 ?>
